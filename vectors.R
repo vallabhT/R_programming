7c39d4005d9274073b180f2b15ad30cae1b766be
vallabT <vallabhtiwari.tiwari@gmail.com>
2020-08-18 19:42:13 +1000

    new commit

---
+++ vectors.R
@@ -0,0 +1,49 @@
+##Vectors in R
+MyFirstVector <- c(3,45,56,743)
+
+MyFirstVector
+
+#is.numeric function to check if an object is numeric or not
+is.numeric(MyFirstVector)
+
+is.integer(MyFirstVector)
+#all the values are stored as an integer
+
+is.double(MyFirstVector
+          )
+
+V2 <- c(3L,12L, 243L, 0L)#Integer vector
+
+is.integer(V2)
+
+#Charcter vector
+
+v3 <- c('A','B',1,4,5)
+is.character(v3)
+v3
+
+
+
+
+
+#sequence
+seq(1,15)  == (1:15)
+
+z <- seq(1,15,4)
+
+d <- rep(3,50)
+d
+
+rep("a",5)
+
+x <- c(80,20)
+y <- rep(x,10)
+y
+
+
+
+
+
+
+
+
